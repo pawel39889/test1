43e156bb5320ace86f73b6a7ac6c8d2201af1586
pawel39889 <pawel39889@gmail.com>
2019-11-27 14:26:54 +0100

    Change convention name, added fillEmailClickButton method to mange all section

--- src/test/java/com/objectRepository/AuthenticationPage.java
+++ src/test/java/com/objectRepository/AuthenticationPage.java
@@ -6,28 +6,32 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 
 public class AuthenticationPage {
-    private WebDriver driver;
+  private WebDriver driver;
 
-    public AuthenticationPage(WebDriver driver) throws FileNotFoundException {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
+  public AuthenticationPage(WebDriver driver) throws FileNotFoundException {
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
 
-    @FindBy(xpath = "//input[@id='email_create']")
-    WebElement emailInput;
+  @FindBy(xpath = "//input[@id='email_create']")
+  WebElement emailInput;
 
-    @FindBy(xpath = "//button[@id='SubmitCreate']")
-    WebElement createAccountButton;
+  @FindBy(xpath = "//button[@id='SubmitCreate']")
+  WebElement createAccountButton;
 
+  public void fillEmailClickButton(HashMap data) {
+    fillEmailInput((String) data.get("email"));
+    clickCreateAccountButton();
+  }
 
+  public void fillEmailInput(String email) {
+    emailInput.sendKeys(email);
+  }
 
-    public WebElement emailInput() {
-        return emailInput;
-    }
-
-    public WebElement createAccountButton() {
-        return createAccountButton;
-    }
+  public void clickCreateAccountButton() {
+    createAccountButton.click();
+  }
 }
