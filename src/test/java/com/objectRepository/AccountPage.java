5a37c0940436d574a4292503e78b86135273a0d9
pawel39889 <pawel39889@gmail.com>
2019-11-27 16:03:23 +0100

    New class AccountPage added to objectRepository

---
+++ src/test/java/com/objectRepository/AccountPage.java
@@ -0,0 +1,29 @@
+package com.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.HashMap;
+
+public class AccountPage {
+  private WebDriver driver;
+
+  public AccountPage(WebDriver driver) {
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
+
+  @FindBy(xpath = "//a[@class='account']")
+  WebElement customerAccountLink;
+
+  public boolean loginVerification(HashMap data) {
+    String username = data.get("name") + " " + data.get("lastName");
+    if (customerAccountLink.getText().equals(username)) {
+      return true;
+    }
+
+    return false;
+  }
+}
