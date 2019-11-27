c73085f7d88cfa14aa05bdf4c9577bcb42f45bd6
pawel39889 <pawel39889@gmail.com>
2019-11-27 12:09:03 +0100

    Added Webdriver Managaer to Registration.class

--- src/test/java/com/testCases/Registration.java
+++ src/test/java/com/testCases/Registration.java
@@ -2,21 +2,51 @@ package com.testCases;
 
 import com.dataImport.DataContainer;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class Registration {
 
-  public Registration() throws FileNotFoundException {}
+  private WebDriver driver;
+
+  @BeforeClass
+  public static void setupClass() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @Before
+  public void setupTest() {
+    driver = new ChromeDriver();
+  }
+
+  @After
+  public void teardown() {
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+
+  //public Registration() throws FileNotFoundException {}
 
   @Test
   public void registration() throws InterruptedException, IOException {
-    String filePath = "src\\test\\java\\data.properties";
+    String filePath = "src\\test\\resources\\data.properties";
 
     HashMap dataFromProperties = DataContainer.getPropertiesHashMap(filePath);
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get("http://automationpractice.com/index.php");
+
+
     /*
 
     //Driver settings
