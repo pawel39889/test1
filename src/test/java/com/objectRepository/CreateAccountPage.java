35186127d0f1b85ffc3e19a027591c3247755202
pawel39889 <pawel39889@gmail.com>
2019-11-27 13:49:30 +0100

    CreateAccuntPage.java - added begin of loading data from properties file

--- src/test/java/com/objectRepository/CreateAccountPage.java
+++ src/test/java/com/objectRepository/CreateAccountPage.java
@@ -5,194 +5,202 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.HashMap;
+
 public class CreateAccountPage {
-    private WebDriver driver;
+  private WebDriver driver;
 
-    public CreateAccountPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
+  public CreateAccountPage(WebDriver driver) {
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
 
-    //Method fills all form
-    public void fillForm() {
-        this.fillTitle();
-        this.fillFirstNameCustomer("Janusz");
-        this.fillLastNameCustomer("Kozak");
-        //this.fillEmail("p@p.pl");
-        this.fillPassword("asdasd123");
-        this.fillDate("11","November","1980");
-        this.fillAddressName("Janusz");
-        this.fillAddressLastname("Kozak");
-        this.fillCompanyName("Mustaffa");
-        this.fillAddress1("ul. Nadbystrzycka");
-        this.fillCity("Lublin");
-        this.fillState("Alaska");
-        this.fillZipCode("80210");
-        this.fillCountry("UnitedStates");
-        this.fillMobilePhone("500 500 500");
-    }
+  // Elements from Create Account Page
+
+  @FindBy(xpath = "//input[@id='id_gender1']")
+  WebElement titleMr;
+
+  @FindBy(xpath = "//input[@id='id_gender2']")
+  WebElement titleMrs;
+
+  @FindBy(xpath = "//input[@id='customer_firstname']")
+  WebElement firstNameCustomer;
+
+  @FindBy(xpath = "//input[@id='customer_lastname']")
+  WebElement lastNameCustomer;
+
+  @FindBy(xpath = "//input[@id='email']") // check it by getting value from this place
+  WebElement email;
+
+  @FindBy(xpath = "//input[@id='passwd']")
+  WebElement password;
+
+  @FindBy(xpath = "//select[@id='days']")
+  WebElement days;
+
+  @FindBy(xpath = "//select[@id='months']")
+  WebElement months;
+
+  @FindBy(xpath = "//select[@id='years']")
+  WebElement years;
+
+  @FindBy(xpath = "//input[@id='firstname']")
+  WebElement addressName;
+
+  @FindBy(xpath = "//input[@id='lastname']")
+  WebElement addressLastname;
+
+  @FindBy(xpath = "//input[@id='company']")
+  WebElement companyName;
+
+  @FindBy(xpath = "//input[@id='address1']")
+  WebElement addressFirstLine;
+
+  @FindBy(xpath = "//input[@id='address2']")
+  WebElement addressSecondLine;
+
+  @FindBy(xpath = "//input[@id='city']")
+  WebElement city;
+
+  @FindBy(xpath = "//select[@id='id_state']")
+  WebElement state;
+
+  @FindBy(xpath = "//input[@id='postcode']")
+  WebElement zipcode;
+
+  @FindBy(xpath = "//select[@id='id_country']")
+  WebElement country;
+
+  @FindBy(xpath = "//textarea[@id='other']")
+  WebElement additionalInformation;
+
+  @FindBy(xpath = "//input[@id='phone']")
+  WebElement homePhone;
+
+  @FindBy(xpath = "//input[@id='phone_mobile']")
+  WebElement mobilePhone;
+
+  @FindBy(xpath = "//input[@id='alias']")
+  WebElement alias;
+
+  @FindBy(xpath = "//button[@id='submitAccount']")
+  WebElement registerButton;
+
+  // Method fills all form
+  public void fillForm(HashMap formData) {
+    HashMap data = formData;
 
 
-    //Elements from Create Account Page
-    @FindBy(xpath = "//input[@id='id_gender1']")
-    WebElement title;
+    fillTitle((String) data.get("title"));
+    fillFirstNameCustomer((String) data.get("name"));
+//    fillLastNameCustomer("Kozak");
+//    this.fillEmail("p@p.pl");
+//    fillPassword("asdasd123");
+//    fillDate("11", "November", "1980");
+//    fillAddressName("Janusz");
+//    fillAddressLastname("Kozak");
+//    fillCompanyName("Mustaffa");
+//    fillAddress1("ul. Nadbystrzycka");
+//    fillCity("Lublin");
+//    fillState("Alaska");
+//    fillZipCode("80210");
+//    fillCountry("UnitedStates");
+//    fillMobilePhone("500 500 500");
+  }
 
-    @FindBy(xpath = "//input[@id='customer_firstname']")
-    WebElement firstNameCustomer;
+  // Methods to interact with all elements
+  public void fillTitle(String title) {
+    if (title.toLowerCase().equals("mr")) titleMr.click();
+    else titleMrs.click();
+  }
 
-    @FindBy(xpath = "//input[@id='customer_lastname']")
-    WebElement lastNameCustomer;
+  public void fillFirstNameCustomer(String name) {
+    this.firstNameCustomer.sendKeys(name);
+  }
 
-    @FindBy(xpath = "//input[@id='email']") //check it by getting value from this place
-    WebElement email;
+  public void fillLastNameCustomer(String lastname) {
+    this.lastNameCustomer.sendKeys(lastname);
+  }
 
-    @FindBy(xpath = "//input[@id='passwd']")
-    WebElement password;
+  public void fillEmail(String email) {
+    this.email.sendKeys(email);
+  }
 
-    @FindBy(xpath = "//select[@id='days']")
-    WebElement days;
+  public void fillPassword(String password) {
+    this.password.sendKeys(password);
+  }
 
-    @FindBy(xpath = "//select[@id='months']")
-    WebElement months;
+  public void fillDate(String day, String month, String year) {
+    this.fillDays(day);
+    this.fillMonths(month);
+    this.fillYears(year);
+  }
 
-    @FindBy(xpath = "//select[@id='years']")
-    WebElement years;
+  public void fillDays(String day) {
+    this.days.sendKeys(day);
+  }
 
-    @FindBy(xpath = "//input[@id='firstname']")
-    WebElement addressName;
+  public void fillMonths(String month) {
+    this.months.sendKeys(month);
+  }
 
-    @FindBy(xpath = "//input[@id='lastname']")
-    WebElement addressLastname;
+  public void fillYears(String year) {
+    this.years.sendKeys(year);
+  }
 
-    @FindBy(xpath = "//input[@id='company']")
-    WebElement companyName;
+  public void fillAddressName(String adressName) {
+    this.addressName.sendKeys(adressName);
+  }
 
-    @FindBy(xpath = "//input[@id='address1']")
-    WebElement addressFirstLine;
+  public void fillAddressLastname(String addressLastname) {
+    this.addressLastname.sendKeys(addressLastname);
+  }
 
-    @FindBy(xpath = "//input[@id='address2']")
-    WebElement addressSecondLine;
+  public void fillCompanyName(String companyName) {
+    this.companyName.sendKeys(companyName);
+  }
 
-    @FindBy(xpath = "//input[@id='city']")
-    WebElement city;
+  public void fillAddress1(String firstLineAddress) {
+    this.addressFirstLine.sendKeys(firstLineAddress);
+  }
 
-    @FindBy(xpath = "//select[@id='id_state']")
-    WebElement state;
+  public void fillAddress2(String secondLineAddress) {
+    this.addressSecondLine.sendKeys(secondLineAddress);
+  }
 
-    @FindBy(xpath = "//input[@id='postcode']")
-    WebElement zipcode;
+  public void fillCity(String city) {
+    this.city.sendKeys(city);
+  }
 
-    @FindBy(xpath = "//select[@id='id_country']")
-    WebElement country;
+  public void fillState(String state) {
+    this.state.sendKeys(state);
+  }
 
-    @FindBy(xpath = "//textarea[@id='other']")
-    WebElement additionalInformation;
+  public void fillZipCode(String zipcode) {
+    this.zipcode.sendKeys(zipcode);
+  }
 
-    @FindBy(xpath = "//input[@id='phone']")
-    WebElement homePhone;
+  public void fillCountry(String country) {
+    this.country.sendKeys(country);
+  }
 
-    @FindBy(xpath = "//input[@id='phone_mobile']")
-    WebElement mobilePhone;
+  public void fillAdditionalInformation(String addiationalInformation) {
+    this.additionalInformation.sendKeys(addiationalInformation);
+  }
 
-    @FindBy(xpath = "//input[@id='alias']")
-    WebElement alias;
+  public void fillHomePhone(String homephone) {
+    this.homePhone.sendKeys(homephone);
+  }
 
-    @FindBy(xpath = "//button[@id='submitAccount']")
-    WebElement registerButton;
+  public void fillMobilePhone(String mobilePhone) {
+    this.mobilePhone.sendKeys(mobilePhone);
+  }
 
-    //Methods to interact with all elements
-    public void fillTitle() {
-        title.click();
-    }
+  public void fillAlias(String alias) {
+    this.alias.sendKeys(alias);
+  }
 
-    public void fillFirstNameCustomer(String name) {
-        this.firstNameCustomer.sendKeys(name);
-    }
-
-    public void fillLastNameCustomer(String lastname) {
-        this.lastNameCustomer.sendKeys(lastname);
-    }
-
-    public void fillEmail(String email) {
-        this.email.sendKeys(email);
-    }
-
-    public void fillPassword(String password) {
-        this.password.sendKeys(password);
-    }
-
-    public void fillDate(String day, String month, String year) {
-        this.fillDays(day);
-        this.fillMonths(month);
-        this.fillYears(year);
-    }
-
-    public void fillDays(String day) {
-        this.days.sendKeys(day);
-    }
-
-    public void fillMonths(String month) {
-        this.months.sendKeys(month);
-    }
-
-    public void fillYears(String year) {
-        this.years.sendKeys(year);
-    }
-
-    public void fillAddressName(String adressName) {
-        this.addressName.sendKeys(adressName);
-    }
-
-    public void fillAddressLastname(String addressLastname) {
-        this.addressLastname.sendKeys(addressLastname);
-    }
-
-    public void fillCompanyName(String companyName) {
-        this.companyName.sendKeys(companyName);
-    }
-
-    public void fillAddress1(String firstLineAddress) {
-        this.addressFirstLine.sendKeys(firstLineAddress);
-    }
-
-    public void fillAddress2(String secondLineAddress) {
-        this.addressSecondLine.sendKeys(secondLineAddress);
-    }
-
-    public void fillCity(String city) {
-        this.city.sendKeys(city);
-    }
-
-    public void fillState(String state) {
-        this.state.sendKeys(state);
-    }
-
-    public void fillZipCode(String zipcode) {
-        this.zipcode.sendKeys(zipcode);
-    }
-
-    public void fillCountry(String country) {
-        this.country.sendKeys(country);
-    }
-
-    public void fillAdditionalInformation(String addiationalInformation) {
-        this.additionalInformation.sendKeys(addiationalInformation);
-    }
-
-    public void fillHomePhone(String homephone) {
-        this.homePhone.sendKeys(homephone);
-    }
-
-    public void fillMobilePhone(String mobilePhone) {
-        this.mobilePhone.sendKeys(mobilePhone);
-    }
-
-    public void fillAlias(String alias) {
-        this.alias.sendKeys(alias);
-    }
-
-    public void clickRegisterButton() {
-        this.registerButton.click();
-    }
-
+  public void clickRegisterButton() {
+    this.registerButton.click();
+  }
 }
